d1bd8b0859b0c3b7b96aff58b37084ac41a9bc21
ZzxDarkxzZGG <ph603719@gmail.com>
2026-02-18 23:55:22 -0300

    Add files via upload
    faltou converte a altura pra cm, desculpa pelo meu erro vou melhorar!!

---
+++ exercicio 6, tentando entender a conversao.py
@@ -0,0 +1,7 @@
+idade = int(input(f"Qual sua idade?"))
+altura = float(input(f"Qual sua altura"))
+soma_mais_5 = idade + 5 
+print(f"Daqui 5 anos vc tera {soma_mais_5} anos")
+print(f"Sua altura em cm e {altura}")
+
+
