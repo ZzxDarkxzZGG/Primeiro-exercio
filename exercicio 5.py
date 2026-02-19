a852c574221d17c28b7acdc2fa9872e018d3a193
ZzxDarkxzZGG <ph603719@gmail.com>
2026-02-18 23:33:52 -0300

    Add files via upload

---
+++ exercicio 5.py
@@ -0,0 +1,6 @@
+Pergunta_1 = print (input(f"Ola qual seu nome e sua idade"))
+Pergunta_2 = print (input(f"Voce acha q tem futuro nisso"))
+if Pergunta_2 == "sim":
+   print("So vai entao n olha pra tras e n pega resposta do chatgpt ta?")
+else:
+   print("Entao estuda mais")
